d73aa7091ced2fc0896d2eee8484968e7f7653b4
vshumilin <vshumilin1488@gmail.com>
2018-10-08 21:38:57 +0500

    Refactor to v2.0

--- parser.py
+++ parser.py
@@ -1,5 +1,6 @@
 import os
 import collections
+import copy
 
 import xlrd
 
@@ -19,68 +20,53 @@ GoldTiger = collections.namedtuple(
         'bag_ipk'
     ]
 )
+# Current order of fields
+# {'Corridor': 0, 'Clevy WareHouse Code': 1, 'Clevy Shipment No': 2,
+# 'Clevy Shipment Date': 3, 'AWB': 4, 'Offloading airport': 5,
+# 'Dispatch No': 6, 'Clevy Batch Number': 7, 'Clevy Box Number': 8,
+# 'UPU Box Number': 9, 'UPU Bag number': 10, 'Bag Gross Weight (in Kg)': 11,
+# 'Number of items in Bag': 12, 'Bag IPK': 13, 'Is bag IPK <= 13 ?': 14}
 
 
-def parse_data(new_dispatch_index, new_high_dispatch_index):
-    excel = '/'
-    while not os.path.isfile(excel):
-        excel = input('Provide path to excel: ')
+def parse_data(dispatch_indexes_dict):
+    clean_data_for_return = []
 
-    workbook = xlrd.open_workbook(excel)
+    header_indexes = {}
+
+    for city in dispatch_indexes_dict:
+        current_city = dispatch_indexes_dict[city]
+        dispatch_indexes_dict[city] = (
+            [x for x in range(int(current_city[0]), int(current_city[1])+1)]
+        )
+    excel_file = '/'
+    while not os.path.isfile(excel_file):
+        excel_file = input('Provide path to excel: ')
+
+    workbook = xlrd.open_workbook(excel_file)
 
     sheet = workbook.sheet_by_index(0)
-    clean_data = []
-    for row in range(1, sheet.nrows):
-        parsed_data = sheet.row_values(row)
-        dispatch_index = parsed_data[6]
-        if parsed_data[5] != 'EKA':
-            continue
-        try:
-            if int(dispatch_index) > new_high_dispatch_index:
-                continue
-            if int(dispatch_index) < new_dispatch_index < int(sheet.row_values(row - 1)[6]):
-                iterator = 0
-                fake_row = row
-                while True:
-                    fake_parser = sheet.row_values(fake_row)
-                    if fake_parser[5] != 'EKA':
-                        continue
-                    if int(fake_parser[6]) != int(sheet.row_values(fake_row + 1)[6]):
-                        break
-                    else:
-                        UPU_bag_number = fake_parser[10]
-                        UPU_box_number = fake_parser[9]
-                        bag_gross_weight = fake_parser[11]
-                        number_of_items_in_bag = fake_parser[12]
-                        bag_ipk = fake_parser[13]
-                        clean_data.append(GoldTiger(
-                            dispatch_num=int(dispatch_index),
-                            UPU_bag=int(UPU_bag_number),
-                            UPU_box=UPU_box_number,
-                            bag_weight=bag_gross_weight,
-                            items_in_bag=int(number_of_items_in_bag),
-                            bag_ipk=bag_ipk
-                        ))
-                    iterator += 1
-                    fake_row += 1
-                row += iterator
-                pass
-            elif int(dispatch_index) < new_dispatch_index:
-                continue
-        except ValueError:
-            continue
-        UPU_bag_number = parsed_data[10]
-        UPU_box_number = parsed_data[9]
-        bag_gross_weight = parsed_data[11]
-        number_of_items_in_bag = parsed_data[12]
-        bag_ipk = parsed_data[13]
-        clean_data.append(GoldTiger(
-            dispatch_num=int(dispatch_index),
-            UPU_bag=int(UPU_bag_number),
-            UPU_box=UPU_box_number,
-            bag_weight=bag_gross_weight,
-            items_in_bag=int(number_of_items_in_bag),
-            bag_ipk=bag_ipk
-        ))
 
-    return clean_data
+    for index, name in enumerate(sheet.row_values(0)):
+        header_indexes[name.replace('\n', '')] = index
+    for city in dispatch_indexes_dict:
+        print(city, dispatch_indexes_dict[city])
+        for dispatch_index in dispatch_indexes_dict[city]:
+            for row in range(1, sheet.nrows):
+                data_in_row = sheet.row_values(row)
+                if data_in_row[6] == dispatch_index and data_in_row[5] == city:
+                    UPU_bag_number = data_in_row[10]
+                    UPU_box_number = data_in_row[9]
+                    bag_gross_weight = data_in_row[11]
+                    number_of_items_in_bag = data_in_row[12]
+                    bag_ipk = data_in_row[13]
+                    clean_data_for_return.append(GoldTiger(
+                        dispatch_num=int(dispatch_index),
+                        UPU_bag=int(UPU_bag_number),
+                        UPU_box=UPU_box_number,
+                        bag_weight=bag_gross_weight,
+                        items_in_bag=int(number_of_items_in_bag),
+                        bag_ipk=bag_ipk
+                    ))
+        dispatch_indexes_dict[city] = copy.deepcopy(clean_data_for_return)
+        clean_data_for_return.clear()
+    return dispatch_indexes_dict
--- writer.py
+++ writer.py
@@ -1,86 +1,99 @@
-import argparse
 import os
 import sys
-from operator import attrgetter
 
 from supports import *
 from parser import parse_data
 
 
-# TODO REFACTOR THIS FOR THE LOVE OF GOD
-def return_parsed_args():
-    parser = argparse.ArgumentParser(description='Process new columns.')
-    parser.add_argument('dispatch_low', metavar='N', type=int, nargs='+',
-                    help='an integer for the parser')
-    parser.add_argument('dispatch_high', metavar='M', type=int, nargs='+',
-                    help='an integer for the parser')
-    args = parser.parse_args()
-    return args
-
-
-def setup_new_sheet():
+def setup_new_sheet(ws):
     ws.row(0).height_mismatch = True
     ws.row(0).height = int(2*260)
     for cell_index, cell_header in enumerate(ROW_HEADER_LIST):
         ws.col(cell_index).width = int((len(cell_header) + 5) * 260)
         ws.write(0, cell_index, cell_header, set_style(SKY_BLUE_COLOR, header=True))
+    del cell_header, cell_index
 
-RAW_CONST = 1
-DISPATCH_INDEXES = set()
+
+PARCEL_INDEXES = {}
 
 if __name__ == '__main__':
-    parsed_args = return_parsed_args()
-    result_of_parser_py = sorted(parse_data(parsed_args.dispatch_low[0], parsed_args.dispatch_high[0]), key=attrgetter('dispatch_num'))
-    # First row is the Main index
-    dispatch_idx = result_of_parser_py[0].dispatch_num
-    wb = XLWTWorkbook()
-    ws = wb.add_sheet(f'S{int(dispatch_idx)}')
 
-    setup_new_sheet()
-    for idx, gold_tiger in enumerate(result_of_parser_py):
-        DISPATCH_INDEXES.add(gold_tiger.dispatch_num)
-        if gold_tiger.dispatch_num != dispatch_idx:
-            RAW_CONST = 1
-            dispatch_idx = gold_tiger.dispatch_num
-            ws = wb.add_sheet(f'S{int(dispatch_idx)}')
-            setup_new_sheet()
-        for inner_index, g_t_data in enumerate(gold_tiger):
-            if RAW_CONST % 2 != 0:
-                try:
-                    ws.col(inner_index).width = int((len(g_t_data) + 4)*260)
-                except TypeError:
-                    pass
-                if isinstance(g_t_data, float):
-                    ws.write(RAW_CONST, inner_index, float(g_t_data), PALE_BLUE_STYLE)
-                elif isinstance(g_t_data, int):
-                    ws.write(RAW_CONST, inner_index, int(g_t_data), PALE_BLUE_STYLE_WO_COMMAS)
+    parcel_destination_list = '/'
+    while not isinstance(parcel_destination_list, list):
+        parcel_destination_list = (
+            (input('Parcel destination via comma(,): ')
+                .replace(' ', '').upper().split(',')
+             )
+        )
+
+    # Make it predictable and append sort for MIN MAX
+    for parcel_city in parcel_destination_list:
+        edge_indexes = []
+        while not len(edge_indexes) > 1:
+            edge_indexes = input(
+                (f'Input lowest and highest index for {parcel_city} '
+                 'dispatches in the following order - min, max: ')
+            ).replace(' ', '').split(',')
+
+        PARCEL_INDEXES[parcel_city] = edge_indexes
+
+    result_of_parser_py = parse_data(PARCEL_INDEXES)
+
+    for parcel_city in result_of_parser_py:
+        # First row is the Main index
+        RAW_CONST = 1
+        dispatch_idx = result_of_parser_py[parcel_city][0].dispatch_num
+        wb = XLWTWorkbook()
+        ws = wb.add_sheet(f'S{int(dispatch_idx)}')
+
+        setup_new_sheet(ws)
+        for idx, gold_tiger in enumerate(result_of_parser_py[parcel_city]):
+            if gold_tiger.dispatch_num != dispatch_idx:
+                RAW_CONST = 1
+                dispatch_idx = gold_tiger.dispatch_num
+                ws = wb.add_sheet(f'S{int(dispatch_idx)}')
+                setup_new_sheet(ws)
+            for inner_index, g_t_data in enumerate(gold_tiger):
+                if RAW_CONST % 2 != 0:
+                    try:
+                        ws.col(inner_index).width = int((len(g_t_data) + 4)*260)
+                    except TypeError:
+                        pass
+                    if isinstance(g_t_data, float):
+                        ws.write(RAW_CONST, inner_index, float(g_t_data), PALE_BLUE_STYLE)
+                    elif isinstance(g_t_data, int):
+                        ws.write(RAW_CONST, inner_index, int(g_t_data), PALE_BLUE_STYLE_WO_COMMAS)
+                    else:
+                        ws.write(RAW_CONST, inner_index, g_t_data, PALE_BLUE_STYLE)
                 else:
-                    ws.write(RAW_CONST, inner_index, g_t_data, PALE_BLUE_STYLE)
+                    try:
+                        ws.col(inner_index).width = int((len(g_t_data) + 4)*260)
+                    except TypeError:
+                        pass
+                    if isinstance(g_t_data, float):
+                        ws.write(RAW_CONST, inner_index, float(g_t_data), WHITE_STYLE)
+                    elif isinstance(g_t_data, int):
+                        ws.write(RAW_CONST, inner_index, int(g_t_data), WHITE_STYLE_WO_COMMAS)
+                    else:
+                        ws.write(RAW_CONST, inner_index, g_t_data, WHITE_STYLE)
+            if gold_tiger.bag_ipk > 13:
+                ws.write(RAW_CONST, len(gold_tiger), '')
             else:
-                try:
-                    ws.col(inner_index).width = int((len(g_t_data) + 4)*260)
-                except TypeError:
-                    pass
-                if isinstance(g_t_data, float):
-                    ws.write(RAW_CONST, inner_index, float(g_t_data), WHITE_STYLE)
-                elif isinstance(g_t_data, int):
-                    ws.write(RAW_CONST, inner_index, int(g_t_data), WHITE_STYLE_WO_COMMAS)
-                else:
-                    ws.write(RAW_CONST, inner_index, g_t_data, WHITE_STYLE)
-        if gold_tiger.bag_ipk > 13:
-            ws.write(RAW_CONST, len(gold_tiger), '')
-        else:
-            ws.write(RAW_CONST, len(gold_tiger), '', LIGHT_GREEN_STYLE)
-        RAW_CONST += 1
+                ws.write(RAW_CONST, len(gold_tiger), '', LIGHT_GREEN_STYLE)
+            RAW_CONST += 1
 
-    wb.worksheets.sort(key=lambda name: int(name.name.replace('S', '')))
-    directory = ''
-    while not os.path.isdir(directory):
-        directory = input('Provide directory to store the file: ')
-    if sys.platform == 'win32':
-        directory = fr'{directory}\\'
-    else:
-        directory = f'{directory}/'
-    wb.save(
-        f'{directory}КОК Депеши S{parsed_args.dispatch_low[0]}-{parsed_args.dispatch_high[0]}.xls'
-    )
+        wb.worksheets.sort(key=lambda name: int(name.name.replace('S', '')))
+        directory = ''
+        while not os.path.isdir(directory):
+            directory = input('Provide directory to store the file: ')
+        if sys.platform == 'win32':
+            directory = fr'{directory}\\'
+        else:
+            directory = f'{directory}/'
+        wb.save(
+            (f'{directory}КОК Депеши '
+             f'S{result_of_parser_py[parcel_city][0].dispatch_num}-'
+             f'{result_of_parser_py[parcel_city][-1].dispatch_num}.xls'
+             )
+        )
+        del wb, ws, idx, gold_tiger, inner_index, g_t_data, dispatch_idx
